bfb9c0360a7d245d989f553b086d0182c86c1ffb
sudonotfound <koxxxm@icloud.com>
2024-10-16 07:41:29 +0900

    first commit

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,11 @@
+for i in 1..20 do
+           if i%3 ==0
+             puts "Fizz"
+           elsif i%5 ==0
+             puts "Buzz"
+           elsif i == 15
+             puts "FizzBuzz"
+           else
+             puts i
+           end
+         end
