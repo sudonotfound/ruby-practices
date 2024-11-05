ef9c40e9e782b88740f5e1a44c726073027925fe
sudonotfound <koxxxm@icloud.com>
2024-11-05 22:25:08 +0900

    fix the program

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,3 +1,20 @@
+require 'debug'
+
+for i in 1..20 do
+  debugger  # ここにブレークポイントを設定
+  if i%15 ==0
+    puts "FizzBuzz"
+  elsif i%3 ==0
+    puts "Fizz"
+  elsif i%5 == 0
+    puts "Buzz"
+  else
+    puts i
+  end
+end
+
+
+=begin
 for i in 1..20 do
            if i%3 ==0
              puts "Fizz"
@@ -9,3 +26,4 @@ for i in 1..20 do
              puts i
            end
          end
+=end
