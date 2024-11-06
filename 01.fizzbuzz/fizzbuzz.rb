cbfa6464c0baeb6a91170a7dbf8e9037dd4d0536
sudonotfound <koxxxm@icloud.com>
2024-11-06 14:47:10 +0900

    remove debug and code formted

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,16 +1,13 @@
 #!/usr/bin/env ruby
 
-require 'debug'
-
 for i in 1..20 do
-  debugger  # ここにブレークポイントを設定
-  if i%15 ==0
-    puts "FizzBuzz"
-  elsif i%3 ==0
-    puts "Fizz"
-  elsif i%5 == 0
-    puts "Buzz"
-  else
-    puts i
-  end
-end
+           if i%15 ==0
+             puts "FizzBuzz"
+           elsif i%3 ==0
+             puts "Fizz"
+           elsif i%5 == 0
+             puts "Buzz"
+           else
+             puts i
+           end
+         end
