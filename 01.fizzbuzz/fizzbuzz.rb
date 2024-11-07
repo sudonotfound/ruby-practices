b000af8be08b885b8119c7b9d100b6a9e744c434
sudonotfound <koxxxm@icloud.com>
2024-11-07 09:31:50 +0900

    add space

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 for i in 1..20 do
-           if i%15 ==0
+           if i % 15 == 0
              puts "FizzBuzz"
-           elsif i%3 ==0
+           elsif i % 3 == 0
              puts "Fizz"
-           elsif i%5 == 0
+           elsif i % 5 == 0
              puts "Buzz"
            else
              puts i
