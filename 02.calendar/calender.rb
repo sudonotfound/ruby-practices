e620cb8136a2d0292983aa3e1f05d59c98d51bad
sudonotfound <koxxxm@icloud.com>
2024-11-07 22:49:49 +0900

    add calender program

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -11,7 +11,7 @@ OptionParser.new do |opt| #クラスの新しいインスタンスを作成し�
   opt.on('-m MONTH', '--month MONTH', 'Month (e.g., 11)') { |o| options[:month] = o.to_i }
 end.parse! #コマンドライン引数の解析を行い、引数で渡された年と月の値がハッシュに保存される
 
-# 年と月のデフォルト値を設定（指定されなかった場合に使用）
+#年と月のデフォルト値を設定（指定されなかった場合に使用）
 year = options[:year] 
 month = options[:month]
 
@@ -37,7 +37,9 @@ first_day.wday はその月の1日が何曜日かを示す数値を返します
 （0が日曜、1が月曜…）。その数に応じた空白を入れることで、1日を正しい位置に表示
 =end
 # 日付を表示
-(first_day..last_day).each do |date| #first_day から last_day までの日付の範囲をイテレートします。各日付オブジェクトを date として順に処理
-  printf("%2d ", date.day) #文字列の「左寄せ」や、数値の「桁のゼロ埋め」や、「小数点以下の桁指定」など、出力する文字列のフォーマットを指定したいときに使用
+(first_day..last_day).each do |date|
+#first_day から last_day までの日付の範囲をイテレートします。各日付オブジェクトを date として順に処理
+  printf("%2d ", date.day)
+  #文字列の「左寄せ」や、数値の「桁のゼロ埋め」や、「小数点以下の桁指定」など、出力する文字列のフォーマットを指定したいときに使用
   puts if date.saturday? #date.saturday? が true（つまり、土曜日）であれば改行を入れます。これにより、土曜日の後に改行し、次の行に移動して表示を続けます。
 end
