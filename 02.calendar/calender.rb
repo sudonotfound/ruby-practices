f047797c46a2de3a5dc0550da9bf63eae3350064
sudonotfound <koxxxm@icloud.com>
2024-11-18 23:01:48 +0900

    fix comment

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -5,9 +5,8 @@ require 'optparse'
 optparseはコマンドラインで引数を用いるために利用する
 =end
 # コマンドライン引数の処理
-options = {} #コマンド引数を格納するためのハッシュ
-OptionParser.new do |opt| #クラスの新しいインスタンスを作成しあげる
-  
+options = {}
+OptionParser.new do |opt|
   opt.on('-y YEAR', '--year YEAR', 'Year (e.g., 2020)') { |o| options[:year] = o.to_i }
   opt.on('-m MONTH', '--month MONTH', 'Month (e.g., 11)') { |o| options[:month] = o.to_i }
 end.parse! #コマンドライン引数の解析を行い、引数で渡された年と月の値がハッシュに保存される
@@ -32,15 +31,9 @@ puts "日 月 火 水 木 金 土"
 
 # 最初の日の曜日に合わせて空白を挿入
 print "   " * first_day.wday
-=begin
-月の最初の日の曜日に合わせて、空白を挿入します。
-first_day.wday はその月の1日が何曜日かを示す数値を返します
-（0が日曜、1が月曜…）。その数に応じた空白を入れることで、1日を正しい位置に表示
-=end
+#月の初日の曜日に応じて空白を入れることで、1日を正しい位置に表示する
 # 日付を表示
 (first_day..last_day).each do |date|
-#first_day から last_day までの日付の範囲をイテレートします。各日付オブジェクトを date として順に処理
   printf("%2d ", date.day)
-  #文字列の「左寄せ」や、数値の「桁のゼロ埋め」や、「小数点以下の桁指定」など、出力する文字列のフォーマットを指定したいときに使用
   puts if date.saturday? #date.saturday? が true（つまり、土曜日）であれば改行を入れます。これにより、土曜日の後に改行し、次の行に移動して表示を続けます。
 end
