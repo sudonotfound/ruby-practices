37db27cdaf3dc0bdf4f507392f607f453826a9f3
sudonotfound <koxxxm@icloud.com>
2024-11-19 22:35:08 +0900

    replace to 10

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,3 +1,4 @@
+=begin
 # コマンドライン引数からカンマ区切りの文字列を取得
 if ARGV.empty?
   puts "引数を指定してください。"
@@ -9,7 +10,7 @@ values = ARGV[0].split(',')
 
 # 結果を表示
 puts "受け取った値: #{values.inspect}"
-=begin
+=end
 #!/usr/bin/env ruby
 
 # コマンドライン引数を取得
@@ -21,10 +22,17 @@ if input_string.nil? || input_string.empty?
   exit 1
 end
 
+def replace_x_with(item)
+  item == 'X' ? 10 : item
+end
+
+input_array = input_string.split(',')
+input_array = input_array.map { |item| replace_x_with(item) }
+input_string = input_array.join(',')
 # 入力された文字列をそのまま出力
 puts input_string
 
-
+=begin
 #フレーム数
 frame = 0
 
@@ -35,40 +43,40 @@ count = 0
 #フレーム毎のスコアを格納する配列
 socre = [0]*max_frame #0が格納された10個のリスト
 
-input_array.length.time do |i|#入力配列の要素に対してループを実行
+input_array.length.times do |i|#入力配列の要素に対してループを実行
   #最終フレームではない
-  if frame +1 != max_frame
-    #ストライクの処理
-    if input_arry[i] == 10
-      if count == 0 #２投目で10ピンを倒した場合
-        #ストライクの時は、1投で２回投げた
-        count =1
-        score[frame] = input_arry[i] + input_arry[i+1]+input_arry[i+2]
-      else
-        score[frame] = input_array[i]+input_array[i+1]
-      end
-     #スペアの場合
-    elsif input_arry[i]+score[frame] + score[frame] == 10
-      score[frame] = score[frame] + input_array[i]+input_array[i+1]
-    #通常の処理
+if frame +1 != max_frame
+  #ストライクの処理
+  if input_array[i] == 10
+    if count == 0 #２投目で10ピンを倒した場合
+      #ストライクの時は、1投で２回投げた
+      count =1
+      score[frame] = input_array[i] + input_array[i+1]+input_arry[i+2]
     else
-      score[frame] += input_array[i]
+      score[frame] = input_array[i]+input_array[i+1]
     end
-    
-
-  count +=1
-
-  if frame + 1 == max_frame
-
+  #スペアの場合
+  elsif input_array[i]+score[frame] + score[frame] == 10
+    score[frame] = score[frame] + input_array[i]+input_array[i+1]
+  #通常の処理
   else
-
-    if count == 2
-      count = 0
-      frame += 1
-    end
+    score[frame] += input_array[i]
   end
 end
+end
+=end
 
-  
+=begin    
+    count +=1
     
+    if frame + 1 == max_frame
+      
+    else
+      
+      if count == 2
+        count = 0
+        frame += 1
+      end
+    end
+  end 
 =end
