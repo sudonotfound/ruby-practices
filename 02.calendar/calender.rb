31a1cd8007f8815ceb150fd28a06baed6a26d5f2
sudonotfound <koxxxm@icloud.com>
2024-12-08 00:47:58 +0900

    remove comment

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -9,13 +9,8 @@ OptionParser.new do |opt|
   opt.on('-m MONTH', '--month MONTH', 'Month (e.g., 11)') { |o| options[:month] = o.to_i }
 end.parse! #コマンドライン引数の解析を行い、引数で渡された年と月の値がハッシュに保存される
 
-#年と月のデフォルト値を設定（指定されなかった場合に使用）
 year = options[:year] 
 month = options[:month]
-
-=begin
-Date.メゾット()
-=end
 
 # 月の最初の日を取得
 first_day = Date.new(year, month, 1)
@@ -23,8 +18,7 @@ first_day = Date.new(year, month, 1)
 # 月の最後の日を取得
 last_day = Date.new(year, month, -1)
 
-# カレンダーのヘッダーを表示
-puts "#{year}年#{month}月".center(20) #.center(20)は年と月を結合した文字列を20文字幅で中央揃え
+puts "#{year}年#{month}月".center(20)
 puts "日 月 火 水 木 金 土"
 
 # 最初の日の曜日に合わせて空白を挿入
@@ -34,5 +28,4 @@ print "   " * first_day.wday
 (first_day..last_day).each do |date|
   printf("%2d ", date.day)
   puts if date.saturday?
-#date.saturday? が true（つまり、土曜日）であれば改行を入れる。(土曜日の後に改行し、次の行に移動して表示を続ける。)
 end
