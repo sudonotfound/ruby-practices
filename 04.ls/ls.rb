fc04ddc9b66bfc92c68cc5741f2b9f7840d2ff9c
sudonotfound <koxxxm@icloud.com>
2024-12-11 16:21:35 +0900

    create ls command without options

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,22 +1,12 @@
 #!/usr/bin/env ruby
 require 'optparse'
 
-options = {}
-OptionParser.new do |opts|
-  opts.on('-l', 'Use a long listing format') { options[:long] = true }
-  opts.on('-a', 'Do not ignore entries starting with .') { options[:all] = true }
-end.parse!
+
 
 dir = ARGV[0] || '.'
 
-entries = Dir.entries(dir).reject { |f| f.start_with?('.') unless options[:all] }
+#entries = Dir.entries(dir).reject { |f| f.start_with?('.') unless options[:all] }
+entries = Dir.entries(dir).reject { |f| f.start_with?('.')}
 
-if options[:long]
-  entries.each do |entry|
-    stat = File.stat(File.join(dir, entry))
-    mode = stat.mode.to_s(8)[-4..-1]
-    puts "#{mode} #{stat.size.to_s.rjust(8)} #{entry}"
-  end
-else
-  puts entries.join("\t")
-end
+puts entries.join("\t")
+
